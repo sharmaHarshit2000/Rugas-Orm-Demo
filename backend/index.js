f01536e4f2119dfcf98b73f388598f3c93b2759f
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-07-02 22:06:26 +0530

    authRoute setup

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+import authRoutes from "./routes/auth.routes.js";
+
 
 dotenv.config();
 const app = express();
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 
 app.get("/", (req, res) => {
